27638bc0a830e9d6575c1319f7620cb3e83e7e70
mastaleru <mastaleru.rafael@gmail.com>
2021-07-14 18:09:13 +0300

    template in data-for example

--- scripts/controllers/demos/TemplateInceptionProblem.js
+++ scripts/controllers/demos/TemplateInceptionProblem.js
@@ -1,21 +1,60 @@
-const { WebcController } = WebCardinal.controllers;
+const {WebcController} = WebCardinal.controllers;
 
 class TemplateInceptionProblem extends WebcController {
     getModel = (_) => ({
         testString: 'This should be visible only in main page and mother template.',
-        arrayTest: [
+        templateName: "mother-template",
+        arrayTest: [{
+            name: 'Rafael',
+            children: [{
+                name: "Maria",
+                children: [{
+                    name: "Adrian"
+                }, {
+                    name: "Viorica",
+                    children:[{
+                        name:"Gheorghe"
+                    }]
+                }]
+            },
+                {
+                    name: "George"
+                }
+            ]
+        },
             {
-                name: 'Rafael'
+                name: 'Tudor',
             },
             {
-                name: 'Cosmin'
+                name: 'Doina',
+                children: [{
+                    name: "Viorel"
+                },
+                    {
+                        name: "Ioan"
+                    }
+                ]
             }
         ]
     });
 
     constructor(element, history) {
         super(element, history);
-        this.setModel(this.getModel());
+        let model = this.getModel();
+
+        let setTemplate = function (person) {
+            if (person.children) {
+                person.templateName = "child-template";
+                person.children.forEach(person => setTemplate(person))
+            } else {
+                person.templateName = "no-child-template";
+            }
+        }
+
+        model.arrayTest.forEach(person=>setTemplate(person))
+        this.model = model;
+
+
     }
 }
 
